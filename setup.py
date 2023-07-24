8f02320d0bc2891df5bfb88deba57334b71e2e91
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2023-07-24 17:01:26 -0400

    Packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import Extension, setup
 grapes_ext = Extension(
     "cgraph",
     sources=["src/grapes/cgraph/cgraph.c", "src/grapes/cgraph/heap.c"],
-    include_dirs=["src/grapes/"],
 )
 
 setup(ext_package="grapes", ext_modules=[grapes_ext])
