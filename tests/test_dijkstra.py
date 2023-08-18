a0fa3f8c43ab565f6c9dc5615155f249e98f1d12
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2023-08-18 19:46:32 -0400

    Add basic tests

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -3,23 +3,64 @@ import grapes
 import unittest
 
 
-class DijkstrasTestCase(unittest.TestCase):
-    def __init__(
-        self, graph: grapes.Multigraph, case_number: int, correct_weight: float
-    ) -> None:
-        super().__init__(f"test{case_number}")
+class DijkstrasCorrectnessTestCase(unittest.TestCase):
+    def __init__(self, graph, src, dst, result) -> None:
+        super().__init__(f"test")
         self.graph = graph
-        self.case = case_number
-        self.correct_weight = correct_weight
+        self.src = src
+        self.dst = dst
+        self.result = result
 
-    def test_correctness(self):
-        grapes_weight = 0
-        self.assertEqual(grapes_weight, self.correct_weight)
+    def test(self):
+        self.assertListEqual(
+            self.graph.shortest_path(
+                self.src, self.dst, algorithm=grapes.ShortestPathAlgorithm.DIJKSTRAS
+            ),
+            self.result,
+        )
+
+
+class DijkstrasErrorTestCase(unittest.TestCase):
+    def test_missing_label(self):
+        g = grapes.LabeledGraph()
+        try:
+            g.shortest_path(1, 2, grapes.ShortestPathAlgorithm.DIJKSTRAS)
+        except Exception as exc:
+            self.assertIsInstance(exc, grapes.GraphMissingNodeError)
+
+    def test_negative_weight(self):
+        g = grapes.LabeledGraph()
+        g.add_node(1)
+        g.add_node(2)
+        try:
+            g.shortest_path(1, 2, grapes.ShortestPathAlgorithm.DIJKSTRAS)
+        except Exception as exc:
+            self.assertIsInstance(exc, grapes.AlgorithmPreconditionError)
 
 
 def suite():
     suite = unittest.TestSuite()
-    suite.addTest(DijkstrasTestCase("test_default_widget_size"))
+
+    g = grapes.LabeledGraph(is_directed=False)
+    g.add_node(1)
+    g.add_node(2)
+    g.add_node(3)
+    g.add_node(4)
+    g.add_node(5)
+
+    g.add_edge(1, 2, weight=0.5)
+    g.add_edge(1, 3, weight=4.0)
+    g.add_edge(2, 3, weight=2.5)
+    g.add_edge(4, 5, weight=1.0)
+
+    suite.addTest(DijkstrasCorrectnessTestCase(g, 1, 2, [1, 2]))
+    suite.addTest(DijkstrasCorrectnessTestCase(g, 1, 3, [1, 2, 3]))
+    suite.addTest(DijkstrasCorrectnessTestCase(g, 4, 5, [4, 5]))
+    suite.addTest(DijkstrasCorrectnessTestCase(g, 5, 4, [5, 4]))
+    suite.addTest(DijkstrasCorrectnessTestCase(g, 1, 4, []))
+
+    suite.addTest(DijkstrasErrorTestCase("test_missing_label"))
+    suite.addTest(DijkstrasErrorTestCase("test_negative_weight"))
     return suite
 
 
