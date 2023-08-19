89678c3161b87897bd34e89eaf273f62f0f54fa1
nouturnsign <89937277+nouturnsign@users.noreply.github.com>
2023-08-18 22:08:30 -0400

    Switch to pytest

--- tests/test_dijkstra.py
+++ tests/test_dijkstra.py
@@ -1,46 +1,9 @@
 import grapes
 
-import unittest
+import pytest
 
 
-class DijkstrasCorrectnessTestCase(unittest.TestCase):
-    def __init__(self, graph, src, dst, result) -> None:
-        super().__init__(f"test")
-        self.graph = graph
-        self.src = src
-        self.dst = dst
-        self.result = result
-
-    def test(self):
-        self.assertListEqual(
-            self.graph.shortest_path(
-                self.src, self.dst, algorithm=grapes.ShortestPathAlgorithm.DIJKSTRAS
-            ),
-            self.result,
-        )
-
-
-class DijkstrasErrorTestCase(unittest.TestCase):
-    def test_missing_label(self):
-        g = grapes.LabeledGraph()
-        try:
-            g.shortest_path(1, 2, grapes.ShortestPathAlgorithm.DIJKSTRAS)
-        except Exception as exc:
-            self.assertIsInstance(exc, grapes.GraphMissingNodeError)
-
-    def test_negative_weight(self):
-        g = grapes.LabeledGraph()
-        g.add_node(1)
-        g.add_node(2)
-        try:
-            g.shortest_path(1, 2, grapes.ShortestPathAlgorithm.DIJKSTRAS)
-        except Exception as exc:
-            self.assertIsInstance(exc, grapes.AlgorithmPreconditionError)
-
-
-def suite():
-    suite = unittest.TestSuite()
-
+def test_dijkstras_correctness():
     g = grapes.LabeledGraph(is_directed=False)
     g.add_node(1)
     g.add_node(2)
@@ -53,17 +16,20 @@ def suite():
     g.add_edge(2, 3, weight=2.5)
     g.add_edge(4, 5, weight=1.0)
 
-    suite.addTest(DijkstrasCorrectnessTestCase(g, 1, 2, [1, 2]))
-    suite.addTest(DijkstrasCorrectnessTestCase(g, 1, 3, [1, 2, 3]))
-    suite.addTest(DijkstrasCorrectnessTestCase(g, 4, 5, [4, 5]))
-    suite.addTest(DijkstrasCorrectnessTestCase(g, 5, 4, [5, 4]))
-    suite.addTest(DijkstrasCorrectnessTestCase(g, 1, 4, []))
-
-    suite.addTest(DijkstrasErrorTestCase("test_missing_label"))
-    suite.addTest(DijkstrasErrorTestCase("test_negative_weight"))
-    return suite
+    assert g.shortest_path(1, 1, grapes.ShortestPathAlgorithm.DIJKSTRAS) == [1]
+    assert g.shortest_path(4, 5, grapes.ShortestPathAlgorithm.DIJKSTRAS) == [4, 5]
+    assert g.shortest_path(5, 4, grapes.ShortestPathAlgorithm.DIJKSTRAS) == [5, 4]
+    assert g.shortest_path(1, 2, grapes.ShortestPathAlgorithm.DIJKSTRAS) == [1, 2]
+    assert g.shortest_path(1, 3, grapes.ShortestPathAlgorithm.DIJKSTRAS) == [1, 2, 3]
+    assert g.shortest_path(1, 4, grapes.ShortestPathAlgorithm.DIJKSTRAS) == []
 
 
-if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    runner.run(suite())
+def test_dijkstras_errors():
+    g = grapes.LabeledGraph()
+    with pytest.raises(grapes.GraphMissingNodeError):
+        g.shortest_path(1, 2, grapes.ShortestPathAlgorithm.DIJKSTRAS)
+    g.add_node(1)
+    g.add_node(2)
+    g.add_edge(1, 2, weight=-1.0)
+    with pytest.raises(grapes.AlgorithmPreconditionError):
+        g.shortest_path(1, 2, grapes.ShortestPathAlgorithm.DIJKSTRAS)
